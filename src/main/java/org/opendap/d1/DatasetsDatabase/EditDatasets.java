1a060357ec95208c465476050ce62daf2c3df7be
James Gallagher <jgallagher@opendap.org>
2014-06-06 00:06:38 +0000

    Removed unused import

--- src/main/java/org/opendap/d1/DatasetsDatabase/EditDatasets.java
+++ src/main/java/org/opendap/d1/DatasetsDatabase/EditDatasets.java
@@ -22,7 +22,7 @@
 package org.opendap.d1.DatasetsDatabase;
 
 import java.io.BufferedReader;
-import java.io.File;
+//import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
