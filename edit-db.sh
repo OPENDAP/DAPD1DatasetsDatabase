e0e113093feba7ba5117ac2c1de973b88bc7d459
James Gallagher <jgallagher@opendap.org>
2014-06-10 18:43:01 +0000

    Fixed a spelling error in the edit-db.sh script 'B' instead of 'b'; it worked on OSX because of the 'case insensitive' filesystem but not on Linux...

--- edit-db.sh
+++ edit-db.sh
@@ -2,7 +2,4 @@
 #
 # Run the Edit Database program for the DAP/D1 servlet. Option -h for help.
 
-#profiler="-javaagent:/Users/jimg/src/jip-src-1.2/profile/profile.jar \
-#-Dprofile.properties=/Users/jimg/src/olfs/resources/profile.properties"
-
-java $profiler -Xms256m -Xmx1024m -jar target/DatasetsDataBase-1.0-SNAPSHOT.jar $*
+java -Xms256m -Xmx1024m -jar target/DatasetsDatabase-1.0-SNAPSHOT.jar $*
